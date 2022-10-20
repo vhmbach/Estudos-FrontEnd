2de0014bf9a92b1388d51351d2806871eaf5fd64
Vitor Bach <vitorhugo.bach@gmail.com>
2022-10-20 00:58:32 -0300

    Pasta para estudos de PHP

---
+++ Aulas-PHP/variaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+       <meta charset="UTF-8">
+       <style></style>
+       <title></title>
+    </head>
+    <body>
+        <div>
+            <?php
+            $n = 4;
+            $nome = "Vitor";
+            echo "$nome";
+            
+            ?>
+        </div>
+    </body>
+</html>
